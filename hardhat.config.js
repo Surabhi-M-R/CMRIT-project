93617e3bf13c0de52227f26a94ac2d9390b4b90e
Surabhi-M-R <surabhimr27@gmail.com>
2025-05-13 01:24:32 +0530

    third commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,12 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: "https://rpc.sepolia.org", // Sepolia RPC endpoint
+      accounts: [process.env.87a9936637113557a582070f346a45644603006dc699d2b23ce5475c79d1ed24] // Your MetaMask private key
+    }
+  }
 };
